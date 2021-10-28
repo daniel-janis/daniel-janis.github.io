183ad4c6d7912a979383ff7a79caacc3f80f20ae
DanJan1999 <danieljanis99@gmail.com>
2021-10-28 10:43:05 -0600

    Education, Skills added, styling up-to-date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import React from "react";
 class Footer extends React.Component {
     render () {
         return (
-            <div className="footer">
-                <div><h2><a href="#top">Back to Top</a></h2></div>
+            <div className="footerContainer">
+                <div className="footer">
+                    <div><h2><a href="#top">Back to Top</a></h2></div>
+                </div>
             </div>
         )
     }
